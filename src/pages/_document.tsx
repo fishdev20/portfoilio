7ad73605679dbccc44240531a6b5c2869e6e1708
Marcus <minh.nguyen@triona.fi>
2023-10-15 23:26:16 +0300

    fix broke view

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta charSet="utf-8"/>
+      <meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+			/>
       <body>
         <Main />
         <NextScript />
